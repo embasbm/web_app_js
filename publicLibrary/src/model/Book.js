c269f44fa478f3469357a8040a9328a8d796f82c
Emba Moussa <emoussa@intellicentrics.com>
2015-03-05 11:55:37 +0000

    Loading all Book instances

--- publicLibrary/src/model/Book.js
+++ publicLibrary/src/model/Book.js
@@ -6,3 +6,22 @@ function Book(slots){
 
 Book.instances = {};
 
+Book.loadAll = function () {
+  var key = "", keys=[], booksString="", books={};
+  try {
+    if (localStorage["Books"]){
+      booksString = localStorage["Books"];
+    }
+  } catch (e) {
+    alert("Error when reading from Local Storage\n" + e);
+  }
+  if (booksString){
+    books = JSON.parse(booksString);
+    keys = Object.keys(books);
+    console.log(keys.length + " books loaded.");
+    for ( i=0; i< keys.length; i++){
+      key=keys[i];
+      Book.instances[key] = Book.convertRow2Obj( books[key]);
+    }
+  }
+};
